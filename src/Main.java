820892ca3a43377ff17e1adc6a9979d8dbc6b5ef
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-21 10:23:11 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter 2 numbers");
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println("Enter 1 for addition");
+        System.out.println("Enter 2 for Subtraction");
+        System.out.println("Enter 3 for Multiplication");
+        System.out.println("Enter 4 for Division");
+
+
+    }
+}
