ee0f2f2de65bb9ddea30242e68624a943dff834a
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-21 11:22:11 +0530

    sfs

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,14 @@ public class Main {
     {
         int b = x - y;
     }
-    static void mult(int x, int y)
+    static void mult()
     {
+        System.out.println("Enter 2 numbers");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         int c = x * y;
+        System.out.println(c);
     }
 
 
